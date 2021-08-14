2870e106597e8c520ca3d9b128dec768e93acb37
Mohammed Rizwan Amanullah <mohrizwan1993@gmail.com>
2021-08-14 18:48:34 +0530

    Updated

--- Intermediate R.r
+++ Intermediate R.r
@@ -45,6 +45,16 @@ views == 13
 views <= 14
 
 ## Logical Operator
+# The linkedin and last variable are already defined for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+last <- tail(linkedin, 1)
+
+# Is last under 5 or above 10?
+last <5 | last > 10
+
+# Is last between 15 (exclusive) and 20 (inclusive)?
+last > 15 & last <=20
+
 #
 #
 #
